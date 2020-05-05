15689cc9c97e7ea644143559637f220306fa8cc3
alexisleonel1 <alexisleogalvan12@gmail.com>
2020-05-05 19:53:05 -0300

    corregido un tipeo

--- views/cars.view.php
+++ views/cars.view.php
@@ -5,9 +5,9 @@ class CarsView{
     public function show_cars($autos){
         foreach ($autos as $auto) {
    
-            echo ' <p>' . $auto->precio . "</p> - ";
-            echo ' <p>' . $auto->titulo . "</p> - ";
-            echo ' <p>' . $auto->nombre_marca.'</p> - ';
+            echo ' <p>' . $auto->precio . "</p>";
+            echo ' <p>' . $auto->titulo . "</p>";
+            echo ' <p>' . $auto->nombre_marca.'</p>';
 
         }
     }
