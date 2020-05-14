c158876abedee0b1d10bb5d73243753ddb349a7c
alexisleonel1 <alexisleogalvan12@gmail.com>
2020-05-14 16:49:25 -0300

    se actualizo la db

--- database/db_autos.sql
+++ database/db_autos.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-05-2020 a las 19:19:26
+-- Tiempo de generación: 14-05-2020 a las 21:48:15
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.4.5
 
@@ -44,7 +44,10 @@ CREATE TABLE `autos` (
 --
 
 INSERT INTO `autos` (`id_auto`, `titulo`, `modelo`, `anio`, `kilometros`, `precio`, `descripcion`, `foto`, `id_marca_fk`) VALUES
-(1, 'Vendo Ford, Focus, modelo 2017, impecable!!! 30.000km', 'Focus', 2017, '30', 400, 'lo vendo impecable!', 'images/focus 2017.jpg', 1);
+(1, 'Vendo Ford, Focus, modelo 2017, impecable!!! 30.000km', 'Focus', 2017, '30', 400, 'lo vendo impecable!', 'images/focus 2017.jpg', 1),
+(9, 'BMW Serie M 5 - 680 HP', 'M 5', 5, '40', 8600000, 'BMW Serie M 2013 5 - 680 HP', 'https://eldiariony.com/wp-content/uploads/sites/2/2015/08/im_20150318_autos_150319375.jpg?quality=80&strip=all&w=940', 3),
+(10, 'Corsa 1.6 4 Pts Super', 'Corsa', 1, '107000', 320000, 'unico dueño\r\nPermuto por algo Ford xfavor!!', 'https://http2.mlstatic.com/corsa-16-4-pts-super-D_NQ_NP_719427-MLA41813048728_052020-F.webp', 2),
+(12, 'Ford Mustang 5.0 Gt 421cv', 'Mustang', 4, '30000', 6200000, 'Venta del usado chequeado por nuestros técnicos para retirar\r\n\r\nTomamos usados en parte de pago y consignación\r\n\r\nContamos con seguros con excelentes beneficios por ser clientes!', 'https://http2.mlstatic.com/ford-mustang-50-gt-421cv-2017-D_NQ_NP_873123-MLA33014927332_112019-F.webp', 1);
 
 -- --------------------------------------------------------
 
@@ -63,7 +66,8 @@ CREATE TABLE `marca` (
 
 INSERT INTO `marca` (`id_marca`, `nombre_marca`) VALUES
 (1, 'Ford'),
-(2, 'Chevrolet');
+(2, 'Chevrolet'),
+(3, 'BMW');
 
 --
 -- Índices para tablas volcadas
@@ -90,13 +94,13 @@ ALTER TABLE `marca`
 -- AUTO_INCREMENT de la tabla `autos`
 --
 ALTER TABLE `autos`
-  MODIFY `id_auto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id_auto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `marca`
 --
 ALTER TABLE `marca`
-  MODIFY `id_marca` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id_marca` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Restricciones para tablas volcadas
