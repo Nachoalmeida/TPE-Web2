5883c8851d78d3b3fbb25ffce50e09134ce2d3df
Nacho Almeida <nachoalmeida77@gmail.com>
2020-05-18 21:42:43 -0300

    Se agrego tabla usuarios en DB y se cambio el nombre de la tabla 'marca' a 'marcas'

--- database/db_autos.sql
+++ database/db_autos.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 14-05-2020 a las 21:48:15
--- Versión del servidor: 10.4.11-MariaDB
--- Versión de PHP: 7.4.5
+-- Host: 127.0.0.1
+-- Generation Time: May 19, 2020 at 02:38 AM
+-- Server version: 10.1.38-MariaDB
+-- PHP Version: 7.3.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -18,13 +19,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de datos: `db_autos`
+-- Database: `db_autos`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `autos`
+-- Table structure for table `autos`
 --
 
 CREATE TABLE `autos` (
@@ -32,7 +33,7 @@ CREATE TABLE `autos` (
   `titulo` varchar(200) NOT NULL,
   `modelo` varchar(200) NOT NULL,
   `anio` int(11) NOT NULL,
-  `kilometros` decimal(11,0) NOT NULL,
+  `kilometros` int(11) NOT NULL,
   `precio` int(11) NOT NULL,
   `descripcion` text NOT NULL,
   `foto` varchar(200) NOT NULL,
@@ -40,77 +41,104 @@ CREATE TABLE `autos` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Volcado de datos para la tabla `autos`
+-- Dumping data for table `autos`
 --
 
 INSERT INTO `autos` (`id_auto`, `titulo`, `modelo`, `anio`, `kilometros`, `precio`, `descripcion`, `foto`, `id_marca_fk`) VALUES
-(1, 'Vendo Ford, Focus, modelo 2017, impecable!!! 30.000km', 'Focus', 2017, '30', 400, 'lo vendo impecable!', 'images/focus 2017.jpg', 1),
-(9, 'BMW Serie M 5 - 680 HP', 'M 5', 5, '40', 8600000, 'BMW Serie M 2013 5 - 680 HP', 'https://eldiariony.com/wp-content/uploads/sites/2/2015/08/im_20150318_autos_150319375.jpg?quality=80&strip=all&w=940', 3),
-(10, 'Corsa 1.6 4 Pts Super', 'Corsa', 1, '107000', 320000, 'unico dueño\r\nPermuto por algo Ford xfavor!!', 'https://http2.mlstatic.com/corsa-16-4-pts-super-D_NQ_NP_719427-MLA41813048728_052020-F.webp', 2),
-(12, 'Ford Mustang 5.0 Gt 421cv', 'Mustang', 4, '30000', 6200000, 'Venta del usado chequeado por nuestros técnicos para retirar\r\n\r\nTomamos usados en parte de pago y consignación\r\n\r\nContamos con seguros con excelentes beneficios por ser clientes!', 'https://http2.mlstatic.com/ford-mustang-50-gt-421cv-2017-D_NQ_NP_873123-MLA33014927332_112019-F.webp', 1);
+(1, 'Vendo Focus!!!', 'Focus 3.0', 2018, 35000, 500000, 'Una masaaaa!!!', '', 1);
 
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `marca`
+-- Table structure for table `marcas`
 --
 
-CREATE TABLE `marca` (
+CREATE TABLE `marcas` (
   `id_marca` int(11) NOT NULL,
   `nombre_marca` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Volcado de datos para la tabla `marca`
+-- Dumping data for table `marcas`
 --
 
-INSERT INTO `marca` (`id_marca`, `nombre_marca`) VALUES
-(1, 'Ford'),
-(2, 'Chevrolet'),
-(3, 'BMW');
+INSERT INTO `marcas` (`id_marca`, `nombre_marca`) VALUES
+(1, 'Ford');
+
+-- --------------------------------------------------------
 
 --
--- Índices para tablas volcadas
+-- Table structure for table `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `mail` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `user_name` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `mail`, `password`, `user_name`) VALUES
+(1, 'admin@gmail.com', '1234', 'carsadmin');
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- Indices de la tabla `autos`
+-- Indexes for table `autos`
 --
 ALTER TABLE `autos`
   ADD PRIMARY KEY (`id_auto`),
-  ADD KEY `id_marca` (`id_marca_fk`);
+  ADD KEY `id_marca_fk` (`id_marca_fk`);
 
 --
--- Indices de la tabla `marca`
+-- Indexes for table `marcas`
 --
-ALTER TABLE `marca`
+ALTER TABLE `marcas`
   ADD PRIMARY KEY (`id_marca`);
 
 --
--- AUTO_INCREMENT de las tablas volcadas
+-- Indexes for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT de la tabla `autos`
+-- AUTO_INCREMENT for table `autos`
 --
 ALTER TABLE `autos`
-  MODIFY `id_auto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_auto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT de la tabla `marca`
+-- AUTO_INCREMENT for table `marcas`
 --
-ALTER TABLE `marca`
-  MODIFY `id_marca` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `marcas`
+  MODIFY `id_marca` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- Restricciones para tablas volcadas
+-- AUTO_INCREMENT for table `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
 --
 
 --
--- Filtros para la tabla `autos`
+-- Constraints for table `autos`
 --
 ALTER TABLE `autos`
-  ADD CONSTRAINT `autos_ibfk_1` FOREIGN KEY (`id_marca_fk`) REFERENCES `marca` (`id_marca`);
+  ADD CONSTRAINT `autos_ibfk_1` FOREIGN KEY (`id_marca_fk`) REFERENCES `marcas` (`id_marca`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
